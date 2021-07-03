4e1ccbe04276148309000f668672e23c8d84bc29
Amit Jain <amitasn@gmail.com>
2021-07-03 23:44:41 +0530

    feat: updated index.html

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const addButton = document.querySelector("#btn-add");
-const inputExpense = document.querySelector("#inputExpense");
+const inputElement = document.querySelector("#inputExpense");
 const inputExpenseType = document.querySelector("#expenseType");
-let counter = 0;
+const totalAmount = document.querySelector("#heading-total");
+const expenseTable = document.querySelector("#expenseTable");
+const allExpenseTable = document.querySelector("#table"); 
+let expenseAmount = 0;
 
-function increment(params) {
-    counter = inputExpense.value;
-    ExpenseType = inputExpense.value;
-    console.log({ExpenseType})
-    console.log(counter);
+totalAmount.textContent = expenseAmount;
+
+//create an array to store all expenses
+
+const allExpenses = [];
+
+function addExpense(params) {
+    const expenseItem = {};
+    const textAmount = inputElement.value;
+    const expenseType = inputExpenseType.value;
+    const expense = parseInt(textAmount,10);
+
+    expenseItem.desc = expenseType;
+    expenseItem.amount = expense;
+
+    allExpenses.push(expenseItem);
+    expenseAmount = expenseAmount + expense;
+    totalAmount.textContent = expenseAmount;
+   
+    allExpenseTable.innerHTML =  allExpenses.map( data => {
+      return `<div>
+      <div>${data.desc} :: ${data.amount}</div>
+      </div>`
+    }).join("");
+
+    
+
+    
 }
-addButton.addEventListener("click",increment);
+addButton.addEventListener("click",addExpense);
 
 
